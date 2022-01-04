826eeb1d8965971d55d0f5475e9cbd5e689f11e4
Carlos Duran <45047071+ndsuero@users.noreply.github.com>
2022-01-03 21:34:55 -0500

    First Commit

---
+++ OdeToFood.Core/CuisineType.cs
@@ -0,0 +1,11 @@
+﻿namespace OdeToFood.Core
+{
+    public enum CuisineType
+    {
+        None,
+        Mexican,
+        Dominican,
+        Indian,
+        Italian
+    }
+}
---
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        //These represent the kind of data, we going to use like storage.
+        public  DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
